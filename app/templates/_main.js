6f8254f41c073651e648b49067612bdaa71fe27a
Joe Nelson <cred+github@begriffs.com>
2013-08-05 22:01:21 -0700

    grunt test already runs jshint

--- app/templates/_main.js
+++
@@ -1,9 +0,0 @@
-if (typeof define !== 'function') {
-  /* jshint latedef:false */
-  var define = require('amdefine')(module);
-}
-
-define(function () {
-  // TODO add your code here, e.g.
-  // foo: function() { }
-});
